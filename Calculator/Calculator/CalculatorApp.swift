e597617c95b35963ce35ea08641a14f8f424713b
Mini <minnnidev@gmail.com>
2023-03-22 21:05:55 +0900

    [Add] 프로젝트 생성

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 김민 on 2023/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
