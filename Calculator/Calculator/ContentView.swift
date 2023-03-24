7652fbb9d4b9cca393dfbe46815aa5f35308cb97
Mini <minnnidev@gmail.com>
2023-03-24 20:23:33 +0900

    [Refactor] #1 - enum 활용하여 버튼 속성 정의

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -7,15 +7,83 @@
 
 import SwiftUI
 
+enum ButtonType: String {
+    case first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, zero
+    case comma, equal, plus, minus, multiple, divide
+    case percent, opposite, clear
+    
+    var buttonDisplayName: String {
+        switch self {
+        case .first:
+            return "1"
+        case .second:
+            return "2"
+        case .third:
+            return "3"
+        case .fourth:
+            return "4"
+        case .fifth:
+            return "5"
+        case .sixth:
+            return "6"
+        case .seventh:
+            return "7"
+        case .eighth:
+            return "8"
+        case .nineth:
+            return "9"
+        case .zero:
+            return "0"
+        case .comma:
+            return ","
+        case .equal:
+            return "="
+        case .plus:
+            return "+"
+        case .minus:
+            return "-"
+        case .multiple:
+            return "X"
+        case .divide:
+            return "/"
+        case .percent:
+            return "%"
+        case .opposite:
+            return "+/-"
+        case .clear:
+            return "C"
+        }
+    }
+        
+    var backgroundColor: Color {
+        switch self {
+        case .first, .second, .third, .fourth, .fifth, .sixth, .seventh, .eighth, .nineth, .zero, .comma:
+            return Color("NumberButtonColor")
+        case .divide, .multiple, .minus, .plus, .equal:
+            return .orange
+        case .clear, .opposite, .percent:
+            return .gray
+        }
+    }
+    
+    var foregroundColor: Color {
+        switch self {
+        case .clear, .opposite, .percent:
+            return .black
+        default:
+            return .white
+        }
+    }
+}
+
 struct ContentView: View {
     @State private var totalNumber: String = "0"
-    private let buttonData: [[String]] = [
-                                            ["C", "+/-", "%", "/"],
-                                            ["7", "8", "9", "X"],
-                                            ["4", "5", "6", "-"],
-                                            ["1", "2", "3", "+"],
-                                            ["0", "0", ".", "="]
-                                    ]
+    private let buttonData: [[ButtonType]] = [
+        [.clear, .opposite, .percent, .divide],
+        [.seventh, .eighth, .nineth, .multiple],
+        [.fourth, .fifth, .sixth, .minus],
+        [.first, .second, .third, .plus],
+        [.zero, .zero, .comma, .equal]]
     
     var body: some View {
         ZStack {
@@ -36,11 +104,11 @@ struct ContentView: View {
                         ForEach(line, id: \.self) { item in
                             Button {
                             } label:    {
-                                Text(item)
+                                Text(item.buttonDisplayName)
                                     .frame(width: 80, height: 80)
-                                    .background(Color("NumberButtonColor"))
+                                    .background(item.backgroundColor)
                                     .cornerRadius(40)
-                                    .foregroundColor(.white)
+                                    .foregroundColor(item.foregroundColor)
                                     .font(.system(size: 33))
                             }
                         }
