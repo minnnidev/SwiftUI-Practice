da9acc0a9dbaf7c324ad84f67cd9cda37236861a
Mini <minnnidev@gmail.com>
2023-04-06 00:04:43 +0900

    [Feat] #4 - scrollView 구현

---
+++ Basic/Basic/LV1/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Basic
+//
+//  Created by 김민 on 2023/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                Text("1")
+                    .frame(width: 300, height: 500)
+                    .background(.red)
+                Text("2")
+                    .frame(width: 300, height: 500)
+                    .background(.orange)
+                Text("3")
+                    .frame(width: 300, height: 500)
+                    .background(.yellow)
+            }
+        }
+        .background(.black)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
