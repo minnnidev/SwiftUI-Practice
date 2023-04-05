e8414af7dce4fec855ca95e0e277ec64bc69d06b
Mini <minnnidev@gmail.com>
2023-04-05 23:51:10 +0900

    [Setting] #4 - 프로젝트 추가

---
+++ Basic/Basic/BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicApp.swift
+//  Basic
+//
+//  Created by 김민 on 2023/04/05.
+//
+
+import SwiftUI
+
+@main
+struct BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
