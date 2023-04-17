6d6cb537d723dd329bcd9f427252e9c3f3cc1df9
Mini <minnnidev@gmail.com>
2023-04-17 20:56:57 +0900

    [Setting] #6 - 프로젝트 생성

---
+++ SwiftUI-Lab/SwiftUI-Lab/SwiftUI_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LabApp.swift
+//  SwiftUI-Lab
+//
+//  Created by 김민 on 2023/04/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
