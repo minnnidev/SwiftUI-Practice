9c276bd3489752f35a4a77dd0d7b1b08d63760e1
Mini <minnnidev@gmail.com>
2023-04-17 21:14:44 +0900

    [Feat] #6 - 당근마켓 탭뷰 따라해 보기

---
+++ SwiftUI-Lab/SwiftUI-Lab/Daangn/DaangnNearMeView.swift
@@ -0,0 +1,23 @@
+//
+//  DaangnNearMeView.swift
+//  SwiftUI-Lab
+//
+//  Created by 김민 on 2023/04/17.
+//
+
+import SwiftUI
+
+struct DaangnNearMeView: View {
+    var body: some View {
+        ZStack {
+            Color.orange
+            Text("내 근처")
+        }
+    }
+}
+
+struct DaangnNearMeView_Previews: PreviewProvider {
+    static var previews: some View {
+        DaangnNearMeView()
+    }
+}
