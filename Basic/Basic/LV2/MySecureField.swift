0b4dc6cdeb6ad850573eeec4c181ba2096d4d3b4
Mini <minnnidev@gmail.com>
2023-04-18 23:19:44 +0900

    [Feat] #5 - 암호화된 입력받기

---
+++ Basic/Basic/LV2/MySecureField.swift
@@ -0,0 +1,20 @@
+//
+//  MySecuredField.swift
+//  Basic
+//
+//  Created by 김민 on 2023/04/18.
+//
+
+import SwiftUI
+
+struct MySecureField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MySecuredField_Previews: PreviewProvider {
+    static var previews: some View {
+        MySecureField()
+    }
+}
