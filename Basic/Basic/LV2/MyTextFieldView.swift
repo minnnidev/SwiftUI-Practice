13d2c497e901699f3f4b79fb0bd30688fc6ffd6a
Mini <minnnidev@gmail.com>
2023-04-18 23:12:18 +0900

    [Feat] #5 - textField 추가해 보기

---
+++ Basic/Basic/LV2/MyTextFieldView.swift
@@ -0,0 +1,20 @@
+//
+//  MyTextFieldView.swift
+//  Basic
+//
+//  Created by 김민 on 2023/04/18.
+//
+
+import SwiftUI
+
+struct MyTextFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyTextFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyTextFieldView()
+    }
+}
