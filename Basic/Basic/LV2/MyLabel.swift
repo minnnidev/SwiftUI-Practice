ec10d3de1ec0112679307d8c36d156f653e6f047
Mini <minnnidev@gmail.com>
2023-04-18 23:31:54 +0900

    [Feat] #5 - Label 사용

---
+++ Basic/Basic/LV2/MyLabel.swift
@@ -0,0 +1,20 @@
+//
+//  MyLabel.swift
+//  Basic
+//
+//  Created by 김민 on 2023/04/18.
+//
+
+import SwiftUI
+
+struct MyLabel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyLabel_Previews: PreviewProvider {
+    static var previews: some View {
+        MyLabel()
+    }
+}
