bf977cb1b7f4d57cd57fdfb810d99b1348964abd
Mini <minnnidev@gmail.com>
2023-04-18 23:26:57 +0900

    [Feat] #5 - progressView 사용

---
+++ Basic/Basic/LV2/MyProgress.swift
@@ -0,0 +1,20 @@
+//
+//  MyProgress.swift
+//  Basic
+//
+//  Created by 김민 on 2023/04/18.
+//
+
+import SwiftUI
+
+struct MyProgress: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyProgress_Previews: PreviewProvider {
+    static var previews: some View {
+        MyProgress()
+    }
+}
